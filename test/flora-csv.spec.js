6cada9dcdf7ccabeb8eec6f943135627e437d32f
Nico Kaiser <nico.kaiser@boerse-go.de>
2018-10-30 09:46:05 +0100

    Update dependencies and code style

--- test/flora-csv.spec.js
+++ test/flora-csv.spec.js
@@ -1,37 +1,39 @@
+/* global describe, it, beforeEach */
+
 'use strict';
 
-var chai = require('chai');
-var expect = chai.expect;
-var FloraCsv = require('../index');
-var bunyan = require('bunyan');
+const { expect } = require('chai');
+const bunyan = require('bunyan');
 
-var api = {
-    log: bunyan.createLogger({name: 'null', streams: []})
+const FloraCsv = require('../index');
+
+const api = {
+    log: bunyan.createLogger({ name: 'null', streams: [] })
 };
 
-describe('flora-csv DataSource', function () {
-    describe('interface', function () {
-        var ds = new FloraCsv(api);
-        it('should export a query function', function () {
+describe('flora-csv DataSource', () => {
+    describe('interface', () => {
+        const ds = new FloraCsv(api);
+        it('should export a query function', () => {
             expect(ds.process).to.be.a('function');
         });
 
-        it('should export a prepare function', function () {
+        it('should export a prepare function', () => {
             expect(ds.prepare).to.be.a('function');
         });
     });
 
-    describe('csv parsing', function () {
-        var ds;
-        var request;
-        var csv = [
+    describe('csv parsing', () => {
+        let ds;
+        let request;
+        const csv = [
             'id;name;birthday',
             '1;Alice;1979-12-31',
             '2;Bob;1977-06-11',
             '3;Horst;1990-01-02'
         ].join('\n');
 
-        beforeEach(function () {
+        beforeEach(() => {
             ds = new FloraCsv(api);
             request = {
                 delimiter: ';',
@@ -40,9 +42,9 @@ describe('flora-csv DataSource', function () {
             };
         });
 
-        it('should return the parsed CSV data', function (done) {
+        it('should return the parsed CSV data', (done) => {
             ds.prepare(request);
-            ds.process(request, function (err, results) {
+            ds.process(request, (err, results) => {
                 if (err) return done(err);
                 expect(results).to.be.an('object');
                 expect(results.data).to.be.an('array');
@@ -51,110 +53,110 @@ describe('flora-csv DataSource', function () {
                     { id: '2', name: 'Bob', birthday: '1977-06-11' },
                     { id: '3', name: 'Horst', birthday: '1990-01-02' }
                 ]);
-                done();
+                return done();
             });
         });
 
-        describe('attributes', function () {
-            it('should return only selected attributes', function (done) {
+        describe('attributes', () => {
+            it('should return only selected attributes', (done) => {
                 request.attributes = ['id', 'birthday'];
                 ds.prepare(request);
-                ds.process(request, function (err, results) {
+                ds.process(request, (err, results) => {
                     if (err) return done(err);
                     expect(results.data).to.eql([
                         { id: '1', birthday: '1979-12-31' },
                         { id: '2', birthday: '1977-06-11' },
                         { id: '3', birthday: '1990-01-02' }
                     ]);
-                    done();
+                    return done();
                 });
             });
         });
 
-        describe('limit', function () {
-            it('should implement "limit"', function (done) {
+        describe('limit', () => {
+            it('should implement "limit"', (done) => {
                 request.limit = 2;
                 ds.prepare(request);
-                ds.process(request, function (err, results) {
+                ds.process(request, (err, results) => {
                     if (err) return done(err);
                     expect(results.data).to.eql([
                         { id: '1', name: 'Alice', birthday: '1979-12-31' },
                         { id: '2', name: 'Bob', birthday: '1977-06-11' }
                     ]);
-                    done();
+                    return done();
                 });
             });
 
-            it('should implement "page"', function (done) {
+            it('should implement "page"', (done) => {
                 request.limit = 2;
                 request.page = 2;
                 ds.prepare(request);
-                ds.process(request, function (err, results) {
+                ds.process(request, (err, results) => {
                     if (err) return done(err);
                     expect(results.data).to.eql([
                         { id: '3', name: 'Horst', birthday: '1990-01-02' }
                     ]);
-                    done();
+                    return done();
                 });
             });
         });
 
-        describe('filter', function () {
-            it('should work for "equal"', function (done) {
+        describe('filter', () => {
+            it('should work for "equal"', (done) => {
                 request.filter = [[
-                    {attribute: 'id', value: '2', operator: 'equal'}
+                    { attribute: 'id', value: '2', operator: 'equal' }
                 ]];
                 ds.prepare(request);
-                ds.process(request, function (err, results) {
+                ds.process(request, (err, results) => {
                     if (err) return done(err);
                     expect(results).to.be.an('object');
                     expect(results.data).to.be.an('array');
                     expect(results.data).to.eql([
                         { id: '2', name: 'Bob', birthday: '1977-06-11' }
                     ]);
-                    done();
+                    return done();
                 });
             });
 
-            it('should implement AND', function (done) {
+            it('should implement AND', (done) => {
                 request.filter = [[
-                    {attribute: 'id', value: '2', operator: 'equal'},
-                    {attribute: 'name', value: 'Bob', operator: 'equal'},
+                    { attribute: 'id', value: '2', operator: 'equal' },
+                    { attribute: 'name', value: 'Bob', operator: 'equal' },
                 ]];
                 ds.prepare(request);
-                ds.process(request, function (err, results) {
+                ds.process(request, (err, results) => {
                     if (err) return done(err);
                     expect(results).to.be.an('object');
                     expect(results.data).to.be.an('array');
                     expect(results.data).to.eql([
                         { id: '2', name: 'Bob', birthday: '1977-06-11' }
                     ]);
-                    done();
+                    return done();
                 });
             });
 
-            it('should implement AND (empty result)', function (done) {
+            it('should implement AND (empty result)', (done) => {
                 request.filter = [[
-                    {attribute: 'id', value: '2', operator: 'equal'},
-                    {attribute: 'name', value: 'Alice', operator: 'equal'},
+                    { attribute: 'id', value: '2', operator: 'equal' },
+                    { attribute: 'name', value: 'Alice', operator: 'equal' },
                 ]];
                 ds.prepare(request);
-                ds.process(request, function (err, results) {
+                ds.process(request, (err, results) => {
                     if (err) return done(err);
                     expect(results).to.be.an('object');
                     expect(results.data).to.be.an('array');
                     expect(results.data).to.eql([]);
-                    done();
+                    return done();
                 });
             });
 
-            it('should implement OR', function (done) {
+            it('should implement OR', (done) => {
                 request.filter = [
-                    [{attribute: 'id', value: '2', operator: 'equal'}],
-                    [{attribute: 'name', value: 'Alice', operator: 'equal'}],
+                    [{ attribute: 'id', value: '2', operator: 'equal' }],
+                    [{ attribute: 'name', value: 'Alice', operator: 'equal' }],
                 ];
                 ds.prepare(request);
-                ds.process(request, function (err, results) {
+                ds.process(request, (err, results) => {
                     if (err) return done(err);
                     expect(results).to.be.an('object');
                     expect(results.data).to.be.an('array');
@@ -162,18 +164,18 @@ describe('flora-csv DataSource', function () {
                         { id: '1', name: 'Alice', birthday: '1979-12-31' },
                         { id: '2', name: 'Bob', birthday: '1977-06-11' }
                     ]);
-                    done();
+                    return done();
                 });
             });
 
-            it('should throw on invalid filter', function (done) {
+            it('should throw on invalid filter', (done) => {
                 request.filter = [[
-                    {attribute: 'id', value: '2', operator: 'greater'}
+                    { attribute: 'id', value: '2', operator: 'greater' }
                 ]];
                 ds.prepare(request);
-                ds.process(request, function (err, results) {
+                ds.process(request, (err) => {
                     expect(err).to.be.an('error');
-                    done();
+                    return done();
                 });
             });
         });
