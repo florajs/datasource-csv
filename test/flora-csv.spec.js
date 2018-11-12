2dd546a0b8c282984f91e367a22d5229d3c75c30
Nico Kaiser <nico.kaiser@boerse-go.de>
2018-11-12 14:22:55 +0100

    Use Prettier for code style

--- test/flora-csv.spec.js
+++ test/flora-csv.spec.js
@@ -26,12 +26,7 @@ describe('flora-csv DataSource', () => {
     describe('csv parsing', () => {
         let ds;
         let request;
-        const csv = [
-            'id;name;birthday',
-            '1;Alice;1979-12-31',
-            '2;Bob;1977-06-11',
-            '3;Horst;1990-01-02'
-        ].join('\n');
+        const csv = ['id;name;birthday', '1;Alice;1979-12-31', '2;Bob;1977-06-11', '3;Horst;1990-01-02'].join('\n');
 
         beforeEach(() => {
             ds = new FloraCsv(api);
@@ -83,45 +78,35 @@ describe('flora-csv DataSource', () => {
                 request.page = 2;
                 ds.prepare(request);
                 const results = await ds.process(request);
-                expect(results.data).to.eql([
-                    { id: '3', name: 'Horst', birthday: '1990-01-02' }
-                ]);
+                expect(results.data).to.eql([{ id: '3', name: 'Horst', birthday: '1990-01-02' }]);
             });
         });
 
         describe('filter', () => {
             it('should work for "equal"', async () => {
-                request.filter = [[
-                    { attribute: 'id', value: '2', operator: 'equal' }
-                ]];
+                request.filter = [[{ attribute: 'id', value: '2', operator: 'equal' }]];
                 ds.prepare(request);
                 const results = await ds.process(request);
                 expect(results).to.be.an('object');
                 expect(results.data).to.be.an('array');
-                expect(results.data).to.eql([
-                    { id: '2', name: 'Bob', birthday: '1977-06-11' }
-                ]);
+                expect(results.data).to.eql([{ id: '2', name: 'Bob', birthday: '1977-06-11' }]);
             });
 
             it('should implement AND', async () => {
-                request.filter = [[
-                    { attribute: 'id', value: '2', operator: 'equal' },
-                    { attribute: 'name', value: 'Bob', operator: 'equal' },
-                ]];
+                request.filter = [
+                    [{ attribute: 'id', value: '2', operator: 'equal' }, { attribute: 'name', value: 'Bob', operator: 'equal' }]
+                ];
                 ds.prepare(request);
                 const results = await ds.process(request);
                 expect(results).to.be.an('object');
                 expect(results.data).to.be.an('array');
-                expect(results.data).to.eql([
-                    { id: '2', name: 'Bob', birthday: '1977-06-11' }
-                ]);
+                expect(results.data).to.eql([{ id: '2', name: 'Bob', birthday: '1977-06-11' }]);
             });
 
             it('should implement AND (empty result)', async () => {
-                request.filter = [[
-                    { attribute: 'id', value: '2', operator: 'equal' },
-                    { attribute: 'name', value: 'Alice', operator: 'equal' },
-                ]];
+                request.filter = [
+                    [{ attribute: 'id', value: '2', operator: 'equal' }, { attribute: 'name', value: 'Alice', operator: 'equal' }]
+                ];
                 ds.prepare(request);
                 const results = await ds.process(request);
                 expect(results).to.be.an('object');
@@ -132,7 +117,7 @@ describe('flora-csv DataSource', () => {
             it('should implement OR', async () => {
                 request.filter = [
                     [{ attribute: 'id', value: '2', operator: 'equal' }],
-                    [{ attribute: 'name', value: 'Alice', operator: 'equal' }],
+                    [{ attribute: 'name', value: 'Alice', operator: 'equal' }]
                 ];
                 ds.prepare(request);
                 const results = await ds.process(request);
@@ -144,16 +129,13 @@ describe('flora-csv DataSource', () => {
                 ]);
             });
 
-            it('should throw on invalid filter', (done) => {
-                request.filter = [[
-                    { attribute: 'id', value: '2', operator: 'greater' }
-                ]];
+            it('should throw on invalid filter', done => {
+                request.filter = [[{ attribute: 'id', value: '2', operator: 'greater' }]];
                 ds.prepare(request);
-                ds.process(request)
-                    .catch((err) => {
-                        expect(err).to.be.an('error');
-                        done();
-                    });
+                ds.process(request).catch(err => {
+                    expect(err).to.be.an('error');
+                    done();
+                });
             });
         });
     });
